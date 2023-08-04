c65f71dd495d9091b978b87f6a4bd6bc122ad251
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-08-04 05:32:04 +0530

    Setup config.ts

---
+++ src/utils/config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+dotenv.config({ path: "./config.env" });
+
+export const PORT = process.env.PORT || 5000;
+export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+export const GOOGLE_OAUTH_TYPE = process.env.GOOGLE_OAUTH_TYPE;
+export const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+export const GOOGLE_OAUTH_CLIENT_SECRET =
+  process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+export const GOOGLE_OAUTH_REFRESH_TOKEN =
+  process.env.GOOGLE_OAUTH_REFRESH_TOKEN;
